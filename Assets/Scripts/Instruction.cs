0d681874a87ea22709174eba8f4a096c17d0392a
Takahiro Fujia <takahirotanakaSJB@gmail.com>
2022-11-17 15:59:01 -0700

    Changes

---
+++ Assets/Scripts/Instruction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Instruction : MonoBehaviour
+{
+    public void Start ()
+     {
+         
+     }
+     
+    public void Update ()
+     {
+         
+     }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+
+    
+}
+
